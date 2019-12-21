b9ef7fd65b7013da2f763c2d7eff8d0462ec3d5d
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-21 01:28:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_supplies[:winter] = {:valentines_day => ["Flags", "Italian Food", "Parade Floats"]}
   
   holiday_supplies[:fall] = {:columbus_day => ["Flags", "Italian Food", "Parade Floats"]}
-  holiday_supplies
 end
 
 def all_winter_holiday_supplies(holiday_hash)
